779a7d922b0d2319cffeddbc6146048729fa76b0
Even <Even@DESKTOP-EK02TT0>
2017-04-23 13:56:31 +0200

    Added start of Conversation

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -14,8 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 import com.ibm.watson.developer_cloud.language_translator.v2.LanguageTranslator;
 import com.ibm.watson.developer_cloud.language_translator.v2.model.Language;
 import com.ibm.watson.developer_cloud.language_translator.v2.model.TranslationResult;
-import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
-import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
 
 /**
 * Servlet implementation class SimpleServlet
